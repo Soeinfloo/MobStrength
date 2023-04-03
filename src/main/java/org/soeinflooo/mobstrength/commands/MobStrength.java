93981758cc0c9f1e08694196f5969a8eeb4d3e7b
Soeinflooo <florianheldt@outlook.de>
2023-04-03 22:52:11 +0200

    Added Reset Command to Completly Reset the Config

--- src/main/java/org/soeinflooo/mobstrength/commands/MobStrength.java
+++ src/main/java/org/soeinflooo/mobstrength/commands/MobStrength.java
@@ -1,5 +1,6 @@
 package org.soeinflooo.mobstrength.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,16 +14,20 @@ public class MobStrength implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         FileConfiguration config = Main.getPlugin().getConfig();
+
         if (sender instanceof Player player) {
-            if(args.length == 0) {
+            if (args.length == 0) {
                 player.sendMessage(Main.prefix + "Please use: /ms <enemy> <modifier> <number>");
-            }else if (args.length == 1) {
-                if(args[0].equals("Reset")){
+
+            } else if (args.length == 1) {
+                if (args[0].equalsIgnoreCase("reset")) {
                     for (String key : config.getConfigurationSection("").getKeys(false)) {
-                        //player.sendMessage(Main.prefix+"Active Mob:"+key);
-                        //player.sendMessage(Main.prefix+config.get(key + ".Damage"));
-                        //player.sendMessage(Main.prefix+config.get(key + ".Modifier"));
+
+                        config.set(key + ".Damage", 0);
+                        config.set(key + ".Modifier", "+");
                     }
+                    player.sendMessage(Main.prefix + "Damage and Modifier of all Mobs has been reset!");
+                    Main.getPlugin().saveConfig();
                 }
 
             } else if (args.length == 2) {
@@ -32,63 +37,64 @@ public class MobStrength implements CommandExecutor {
                 String enemy = args[0];
                 String modifier = args[1];
                 Double damage = Double.valueOf(args[2]);
-                if (player.isOp()) {
 
-                    //If the modifier is "Plus"
-                    if (modifier.equals("+")) {
+                //If the modifier is "Plus"
+                if (modifier.equals("+")) {
 
-                        player.sendMessage(Main.prefix + "Damage from: " + enemy + " has been increased by: " + damage + " damage");
-                        config.set(enemy.toLowerCase() + ".Damage", damage);
-                        config.set(enemy.toLowerCase() + ".Modifier", modifier);
-                        Main.getPlugin().saveConfig();
+                    player.sendMessage(Main.prefix + "Damage from: " + enemy + " has been increased by: " + damage + " damage");
+                    config.set(enemy.toLowerCase() + ".Damage", damage);
+                    config.set(enemy.toLowerCase() + ".Modifier", modifier);
+                    Main.getPlugin().saveConfig();
 
-                        player.sendMessage(String.valueOf(config.get(enemy.toLowerCase())));
-                        player.sendMessage(String.valueOf(config.get(enemy.toLowerCase() + ".Modifier")));
-                        player.sendMessage(String.valueOf(config.get(enemy.toLowerCase() + ".Damage")));
+                    player.sendMessage(String.valueOf(config.get(enemy.toLowerCase())));
+                    player.sendMessage(String.valueOf(config.get(enemy.toLowerCase() + ".Modifier")));
+                    player.sendMessage(String.valueOf(config.get(enemy.toLowerCase() + ".Damage")));
 
-                        //If the modifier is "Minus"
-                    } else if (modifier.equals("-")) {
+                    //If the modifier is "Minus"
+                } else if (modifier.equals("-")) {
 
-                        player.sendMessage(Main.prefix + "Damage from: " + enemy + " has been decreased by: " + damage + " damage");
-                        config.set(enemy.toLowerCase() + ".Damage", damage);
-                        config.set(enemy.toLowerCase() + ".Modifier", modifier);
-                        Main.getPlugin().saveConfig();
+                    player.sendMessage(Main.prefix + "Damage from: " + enemy + " has been decreased by: " + damage + " damage");
+                    config.set(enemy.toLowerCase() + ".Damage", damage);
+                    config.set(enemy.toLowerCase() + ".Modifier", modifier);
+                    Main.getPlugin().saveConfig();
 
-                        player.sendMessage(String.valueOf(Main.prefix + config.get(enemy.toLowerCase())));
-                        player.sendMessage(String.valueOf(Main.prefix + config.get(enemy.toLowerCase() + ".Modifier")));
-                        player.sendMessage(String.valueOf(Main.prefix + config.get(enemy.toLowerCase() + ".Damage")));
+                    player.sendMessage(String.valueOf(Main.prefix + config.get(enemy.toLowerCase())));
+                    player.sendMessage(String.valueOf(Main.prefix + config.get(enemy.toLowerCase() + ".Modifier")));
+                    player.sendMessage(String.valueOf(Main.prefix + config.get(enemy.toLowerCase() + ".Damage")));
 
-                        //If the modifier is "Percent"
-                    } else if (modifier.equals("%")) {
+                    //If the modifier is "Percent"
+                } else if (modifier.equals("%")) {
 
-                        player.sendMessage(Main.prefix + "Damage from: " + enemy + " has been set to: " + damage * 100 + "% damage");
-                        config.set(enemy.toLowerCase() + ".Damage", damage);
-                        config.set(enemy.toLowerCase() + ".Modifier", modifier);
-                        Main.getPlugin().saveConfig();
+                    player.sendMessage(Main.prefix + "Damage from: " + enemy + " has been set to: " + damage * 100 + "% damage");
+                    config.set(enemy.toLowerCase() + ".Damage", damage);
+                    config.set(enemy.toLowerCase() + ".Modifier", modifier);
+                    Main.getPlugin().saveConfig();
 
-                        player.sendMessage(String.valueOf(Main.prefix + config.get(enemy.toLowerCase())));
-                        player.sendMessage(String.valueOf(Main.prefix + config.get(enemy.toLowerCase() + ".Modifier")));
-                        player.sendMessage(String.valueOf(Main.prefix + config.get(enemy.toLowerCase() + ".Damage")));
+                    player.sendMessage(String.valueOf(Main.prefix + config.get(enemy.toLowerCase())));
+                    player.sendMessage(String.valueOf(Main.prefix + config.get(enemy.toLowerCase() + ".Modifier")));
+                    player.sendMessage(String.valueOf(Main.prefix + config.get(enemy.toLowerCase() + ".Damage")));
 
-                        //If the modifier is "set"
-                    } else if (modifier.equals("set")) {
+                    //If the modifier is "set"
+                } else if (modifier.equals("set")) {
 
-                        player.sendMessage(Main.prefix + "Damage from: " + enemy + " has been set to: " + damage + " damage");
-                        config.set(enemy.toLowerCase() + ".Damage", damage);
-                        config.set(enemy.toLowerCase() + ".Modifier", modifier);
-                        Main.getPlugin().saveConfig();
+                    player.sendMessage(Main.prefix + "Damage from: " + enemy + " has been set to: " + damage + " damage");
+                    config.set(enemy.toLowerCase() + ".Damage", damage);
+                    config.set(enemy.toLowerCase() + ".Modifier", modifier);
+                    Main.getPlugin().saveConfig();
 
-                        player.sendMessage(String.valueOf(Main.prefix + config.get(enemy.toLowerCase())));
-                        player.sendMessage(String.valueOf(Main.prefix + config.get(enemy.toLowerCase() + ".Modifier")));
-                        player.sendMessage(String.valueOf(Main.prefix + config.get(enemy.toLowerCase() + ".Damage")));
+                    player.sendMessage(String.valueOf(Main.prefix + config.get(enemy.toLowerCase())));
+                    player.sendMessage(String.valueOf(Main.prefix + config.get(enemy.toLowerCase() + ".Modifier")));
+                    player.sendMessage(String.valueOf(Main.prefix + config.get(enemy.toLowerCase() + ".Damage")));
 
-                    } else return false;
-                }
-            } else {
-                player.sendMessage(Main.prefix + "Please use: /ms <enemy> <modifier> <number>");
+                } else return false;
             }
-        }return false;
+        } else {
+            Bukkit.getLogger().info(Main.prefix+"This Command is 'Player-Only'");
+        }
+        return false;
     }
 }
+
+
 
 
